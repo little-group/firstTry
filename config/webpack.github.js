36dff2375e154f47d318e276d1603fc80db01fee
Dmitri Afonin <stealss7@gmail.com>
2017-09-04 23:18:19 +0300

    duplicate index as 404 for gh pages https://angular.io/guide/deployment#production-servers

--- config/webpack.github.js
+++ config/webpack.github.js
@@ -1,7 +1,10 @@
-var webpackMerge = require('webpack-merge');
-var commonConfig = require('./webpack.common.js');
+let webpackMerge = require('webpack-merge');
+let commonConfig = require('./webpack.common.js');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const { BaseHrefWebpackPlugin } = require('base-href-webpack-plugin'); // Or `import 'base-href-webpack-plugin';` if using typescript
+const entryPoints = ["inline","polyfills","sw-register","scripts","styles","vendor","main"];
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'github';
 
@@ -17,6 +20,34 @@ module.exports = webpackMerge(commonConfig, {
 
 // Add to plugins
 plugins: [
-  new BaseHrefWebpackPlugin({ baseHref: '/firstTry/' })
+  new BaseHrefWebpackPlugin({ baseHref: '/firstTry/' }),
+  new HtmlWebpackPlugin({
+    "template": "./src/index.html",
+    "filename": "./404.html",
+    "hash": false,
+    "inject": true,
+    "compile": true,
+    "favicon": false,
+    "minify": false,
+    "cache": true,
+    "showErrors": true,
+    "chunks": "all",
+    "excludeChunks": [],
+    "title": "Webpack App",
+    "xhtml": true,
+    "chunksSortMode": function sort(left, right) {
+      let leftIndex = entryPoints.indexOf(left.names[0]);
+      let rightindex = entryPoints.indexOf(right.names[0]);
+      if (leftIndex > rightindex) {
+        return 1;
+      }
+      else if (leftIndex < rightindex) {
+        return -1;
+      }
+      else {
+        return 0;
+      }
+    }
+  }),
 ]
 });
